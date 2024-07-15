7668372e0e03446856f0fbbdff3a284c632bbf7f
hyunganon <dlguswo0504@naver,.com>
2024-07-15 16:14:41 +0900

    git init

---
+++ Salsforce-Middleware/src/main/java/com/SalsforceMiddlewareServer/SalsforceMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.SalsforceMiddlewareServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsforceMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalsforceMiddlewareApplication.class, args);
+	}
+
+}
---
+++ Salsforce-Middleware/src/test/java/com/SalsforceMiddlewareServer/SalsforceMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SalsforceMiddlewareServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsforceMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
