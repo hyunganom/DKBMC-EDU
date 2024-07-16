497584f72a7e26d92485198bbfd830a35dcec4e6
hyunganon <dlguswo0504@naver,.com>
2024-07-16 12:11:19 +0900

    hyunjae --add
    서버 설정 클래스 작성 Configuration

---
+++ Salsforce-Middleware/src/main/java/com/SalsforceMiddlewareServer/configuration/BeanConfiguration.java
@@ -0,0 +1,31 @@
+package com.SalsforceMiddlewareServer.configuration;
+
+import com.SalsforceMiddlewareServer.adapters.out.SalesforceAuthenticationAdapter;
+import com.SalsforceMiddlewareServer.adapters.out.SalesforceOutboundPortImpl;
+import com.SalsforceMiddlewareServer.core.ports.out.SalesforceAuthenticationPort;
+import com.SalsforceMiddlewareServer.core.ports.out.SalesforceOutboundPort;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.impl.client.CloseableHttpClient; //Apache HttpClient 구현체.
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class BeanConfiguration { // Salesforce와 상호작용하는 포트를 설정하는 여러 Bean을 정의
+
+    @Bean
+    @Primary // 같은 타입의 빈이 여러 개 있을 때 기본으로 사용될 빈을 지정
+    public SalesforceAuthenticationPort salesforcePort(CloseableHttpClient closeableHttpClient,
+                                                       SalesforceConfigurationProperties salesforceConfigurationProperties,
+                                                       ObjectMapper objectMapper) {
+        return new SalesforceAuthenticationAdapter(closeableHttpClient, salesforceConfigurationProperties, objectMapper);
+    }
+
+    @Bean
+    public SalesforceOutboundPort salesforceOutboundPort(CloseableHttpClient closeableHttpClient,
+                                                         ObjectMapper objectMapper,
+                                                         SalesforceAuthenticationPort salesforceAuthenticationPort) {
+        return new SalesforceOutboundPortImpl(closeableHttpClient, objectMapper, salesforceAuthenticationPort);
+    }
+
+}
