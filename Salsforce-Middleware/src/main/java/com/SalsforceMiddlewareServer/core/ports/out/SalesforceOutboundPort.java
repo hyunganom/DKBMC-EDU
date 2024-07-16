f74aa5ae93f5e81826566145352895e1c87dc603
hyunganon <dlguswo0504@naver,.com>
2024-07-16 12:10:04 +0900

    hyunjae --update 수정
    port 인터페이스 주석 설명 추가

--- Salsforce-Middleware/src/main/java/com/SalsforceMiddlewareServer/core/ports/out/SalesforceOutboundPort.java
+++ Salsforce-Middleware/src/main/java/com/SalsforceMiddlewareServer/core/ports/out/SalesforceOutboundPort.java
@@ -4,4 +4,5 @@ import com.SalsforceMiddlewareServer.core.domain.salesforce.SendToSalesforce;
 
 public interface SalesforceOutboundPort {
     void sendChat(SendToSalesforce sendChat) throws Exception; //객체를 Salesforce로 전송
+
 }
